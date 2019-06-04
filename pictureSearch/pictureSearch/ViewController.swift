8672138be8fb60809c405531c0fed77fc6542d3a
Vladyslav Filipov <filipowlad@gmail.com>
2019-06-04 20:04:27 +0300

    Initial commit.

---
+++ pictureSearch/pictureSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pictureSearch
+//
+//  Created by Vladyslav Filipov on 6/4/19.
+//  Copyright © 2019 Vladyslav Filipov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
